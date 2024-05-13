efb9d4163a0d7ca1f2cce1bddd3216fd85b73a87
MDShakibul <aitldev14@gmail.com>
2024-05-14 00:14:08 +0600

    "Add OurPortfolio component to Home page and adjust related styles"
    This diff adds the `OurPortfolio` component to the `Home` page and adjusts the related styles in `Home.module.css`. The new styles are

---
+++ src/pages/components/UI/OurPortfolio.js
@@ -0,0 +1,112 @@
+import React from "react";
+import style from "../../../styles/Home.module.css";
+import Image from "next/image";
+import DummyImage from "../../../pages/assets/images/dummy-image.svg";
+
+const OurPortfolio = () => {
+  return (
+    <div className={`${style.ourPortfolioSection}`}>
+      <div className={`container ${style.ourPortfolioArea} h-100`}>
+        <div className={`container ${style.ourPortfolioAreaImage} h-100`}>
+          <p className={`${style.heroSectionCompanyTitle}`}>Selected works</p>
+          <h1 className={`footer-nav-link-title ${style.heroSectionCompanyHeader}`}>
+            Discover our portfolio.
+          </h1>
+
+          <div className="row">
+            <div className="col-lg-6">
+              <Image
+                src={DummyImage}
+                alt="panther hero section"
+                layout="responsive"
+                className={`${style.responsiveHeroImage}`}
+              />
+
+              <h3 className={`${style.projectName}`}>Ok Medical</h3>
+              <p className={`${style.projectSummary}`}>
+                Tortor tempor pellentesque tortor urna. Cursus et sed sociis
+                justo vitae at pharetra at nunc. Porta sed et diam dui.
+              </p>
+
+              <div className="d-flex align-items-center">
+                <p className={`${style.projectType}`}>UI/UX DESIGN</p>
+                <div className={`${style.projecTopicDivider}`}></div>
+                <p className={`${style.projectType}`}>E-COMMERCE DEVELOPMENT</p>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <Image
+                src={DummyImage}
+                alt="panther hero section"
+                layout="responsive"
+                className={`${style.responsiveHeroImage} ${style.profileProjectImage}`}
+              />
+
+              <div className={` ${style.profileProjectImageDetails}`}>
+                <h3 className={`${style.projectName}`}>Ok Medical</h3>
+                <p className={`${style.projectSummary}`}>
+                  Tortor tempor pellentesque tortor urna. Cursus et sed sociis
+                  justo vitae at pharetra at nunc. Porta sed et diam dui.
+                </p>
+
+                <div className="d-flex align-items-center">
+                  <p className={`${style.projectType}`}>UI/UX DESIGN</p>
+                  <div className={`${style.projecTopicDivider}`}></div>
+                  <p className={`${style.projectType}`}>
+                    E-COMMERCE DEVELOPMENT
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <Image
+                src={DummyImage}
+                alt="panther hero section"
+                layout="responsive"
+                className={`${style.responsiveHeroImage}`}
+              />
+
+              <h3 className={`${style.projectName}`}>Ok Medical</h3>
+              <p className={`${style.projectSummary}`}>
+                Tortor tempor pellentesque tortor urna. Cursus et sed sociis
+                justo vitae at pharetra at nunc. Porta sed et diam dui.
+              </p>
+
+              <div className="d-flex align-items-center">
+                <p className={`${style.projectType}`}>UI/UX DESIGN</p>
+                <div className={`${style.projecTopicDivider}`}></div>
+                <p className={`${style.projectType}`}>E-COMMERCE DEVELOPMENT</p>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <Image
+                src={DummyImage}
+                alt="panther hero section"
+                layout="responsive"
+                className={`${style.responsiveHeroImage} ${style.profileProjectImage}`}
+              />
+
+              <div className={` ${style.profileProjectImageDetails}`}>
+                <h3 className={`${style.projectName}`}>Ok Medical</h3>
+                <p className={`${style.projectSummary}`}>
+                  Tortor tempor pellentesque tortor urna. Cursus et sed sociis
+                  justo vitae at pharetra at nunc. Porta sed et diam dui.
+                </p>
+
+                <div className="d-flex align-items-center">
+                  <p className={`${style.projectType}`}>UI/UX DESIGN</p>
+                  <div className={`${style.projecTopicDivider}`}></div>
+                  <p className={`${style.projectType}`}>
+                    E-COMMERCE DEVELOPMENT
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OurPortfolio;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Image from "next/image";
 import DummyImage from "../pages/assets/images/dummy-image.svg";
 import Link from "next/link";
 import HeroSection from "./components/UI/HeroSection";
+import OurPortfolio from "./components/UI/OurPortfolio";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       </Head>
 
       <HeroSection/>
+      <OurPortfolio/>
 
       
     </>
